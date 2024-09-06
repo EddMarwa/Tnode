630a906bb01af70e3dc40a349463492526252339
EddMarwa <149029644+EddMarwa@users.noreply.github.com>
2024-09-06 23:48:42 +0300

    1

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
     console.log('Server running at http://127.0.0.1:3000/');
 });
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
+});
+
+app.listen(3000, () => {
+    console.log('Server running at http://localhost:3000');
+});
