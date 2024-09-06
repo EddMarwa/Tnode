ec15139104e2407eea028172065415141a3ec3da
EddMarwa <149029644+EddMarwa@users.noreply.github.com>
2024-09-06 23:55:48 +0300

    dusted

--- app.js
+++ app.js
@@ -4,20 +4,9 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello from Node.js!\n');
+    res.end('Hello from Edd\n');
 });
 
 server.listen(3000, () => {
     console.log('Server running at http://127.0.0.1:3000/');
-});
-
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello from Express!');
-});
-
-app.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
 });
